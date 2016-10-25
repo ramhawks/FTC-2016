502b1f7212be98ede1cf97869cd2b0f6655f7780
Winona Ramhawks <winonaramhawks@gmail.com>
2016-10-25 18:20:15 -0500

    Wrote teleop

---
+++ main/java/org/firstinspires/ftc/teamcode/RamhawkTeleop.java
@@ -0,0 +1,181 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
+ * All device access is managed through the HardwarePushbot class.
+ * The code is structured as a LinearOpMode
+ *
+ * This particular OpMode executes a POV Game style Teleop for a PushBot
+ * In this mode the left stick moves the robot FWD and back, the Right stick turns left and right.
+ * It raises and lowers the claw using the Gampad Y and A buttons respectively.
+ * It also opens and closes the claws slowly using the left and right Bumper buttons.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Ramhawk Teleop", group="Pushbot")
+public class RamhawkTeleop extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private RamhawkHardware robot           = new RamhawkHardware();
+
+    private double          clawOffset      = 0;
+    private final double    CLAW_SPEED      = 0.02 ;
+
+    @Override
+    public void runOpMode() {
+        double left;
+        double right;
+        double max;
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        final View relativeLayout = ((Activity) robot.hwMap.appContext).findViewById(R.id.RelativeLayout);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        boolean colorLedCurrentState;
+        boolean colorLedPreviousState = false;
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Run wheels in POV mode (note: The joystick goes negative when pushed forwards, so negate it)
+            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            left  = -gamepad1.left_stick_y + gamepad1.right_stick_x;
+            right = -gamepad1.left_stick_y - gamepad1.right_stick_x;
+
+            // Normalize the values so neither exceed +/- 1.0
+            max = Math.max(Math.abs(left), Math.abs(right));
+            if (max > 1.0)
+            {
+                left /= max;
+                right /= max;
+            }
+
+            if (robot.distanceSensor.getRawLightDetected() > 0.05) {
+                if (left > 0) left = 0;
+                if (right > 0) right = 0;
+                telemetry.addData("Greater than 0.4?", "yes");
+            } else
+                telemetry.addData("Greater than 0.4?", "no");
+
+
+            robot.leftMotor.setPower(left);
+            robot.rightMotor.setPower(right);
+
+            // Use gamepad left & right Bumpers to open and close the claw
+            if (gamepad1.right_bumper)
+                clawOffset += CLAW_SPEED;
+            else if (gamepad1.left_bumper)
+                clawOffset -= CLAW_SPEED;
+
+            // Move both servos to new position.  Assume servos are mirror image of each other.
+            clawOffset = Range.clip(clawOffset, -0.5, 0.5);
+            robot.leftClaw.setPosition(RamhawkHardware.MID_SERVO + clawOffset);
+            robot.rightClaw.setPosition(RamhawkHardware.MID_SERVO - clawOffset);
+
+            // Use gamepad buttons to move arm up (Y) and down (A)
+            if (gamepad1.y)
+                robot.armMotor.setPower(RamhawkHardware.ARM_UP_POWER);
+            else if (gamepad1.a)
+                robot.armMotor.setPower(RamhawkHardware.ARM_DOWN_POWER);
+            else
+                robot.armMotor.setPower(0.0);
+
+            colorLedCurrentState = gamepad1.x;
+
+            // check for button state transitions.
+
+            telemetry.addData("X", gamepad1.x ? "yes" : "no");
+            if (colorLedCurrentState && !colorLedPreviousState)  {
+                // button is transitioning to a pressed state. So Toggle LED
+                robot.ledOn = !robot.ledOn;
+                robot.colorSensor.enableLed(robot.ledOn);
+            }
+
+            colorLedPreviousState = colorLedCurrentState;
+
+            Color.RGBToHSV(robot.colorSensor.red() * 8, robot.colorSensor.green() * 8, robot.colorSensor.blue() * 8, robot.hsvValues);
+
+            // Send telemetry message to signify robot running;
+            telemetry.addData("claw",  "Offset = %.2f", clawOffset);
+            telemetry.addData("left",  "%.2f", left);
+            telemetry.addData("right", "%.2f", right);
+
+            telemetry.addData("Raw",    robot.distanceSensor.getRawLightDetected());
+            telemetry.addData("Normal", robot.distanceSensor.getLightDetected());
+            telemetry.addData("Raw Max", robot.distanceSensor.getRawLightDetectedMax());
+
+            telemetry.addData("LED", robot.ledOn ? "On" : "Off");
+            telemetry.addData("Clear", robot.colorSensor.alpha());
+            telemetry.addData("Red  ", robot.colorSensor.red());
+            telemetry.addData("Green", robot.colorSensor.green());
+            telemetry.addData("Blue ", robot.colorSensor.blue());
+            telemetry.addData("Hue", robot.hsvValues[0]);
+            telemetry.addData("Saturation", robot.hsvValues[1]);
+            telemetry.addData("Value", robot.hsvValues[2]);
+
+            /*relativeLayout.post(new Runnable() {
+                public void run() {
+                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, robot.hsvValues));
+                }
+            });*/
+
+            telemetry.update();
+
+            // Pause for metronome tick.  40 mS each cycle = update 25 times a second.
+            robot.waitForTick(40);
+        }
+    }
+}
