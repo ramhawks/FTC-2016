99bc9df64296dbb236b2db5ee9421889da7f270d
Winona Ramhawks <winonaramhawks@gmail.com>
2016-11-03 20:22:49 -0500

    Incorporated the ability to change motor thrust in robot test drive mode and a convenient method of changing variables on the driver station

--- main/java/org/firstinspires/ftc/teamcode/TestDrive.java
+++ main/java/org/firstinspires/ftc/teamcode/TestDrive.java
@@ -8,8 +8,12 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class TestDrive extends LinearOpMode {
     private RamhawkHardware robot;
     private double driveTime;
+    private double speed;
     private boolean yBefore;
     private boolean aBefore;
+    private boolean lbBefore;
+    private boolean rbBefore;
+    private boolean index;
     private ElapsedTime timer;
     private boolean init = false;
 
@@ -17,11 +21,12 @@ public class TestDrive extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         waitForStart();
 
-        timer = new ElapsedTime(0);
+        timer = new ElapsedTime();
+        timer.reset();
 
         while (opModeIsActive() && timer.seconds() <= driveTime) {
-            robot.leftMotor.setPower(0.45);
-            robot.rightMotor.setPower(0.45);
+            robot.leftMotor.setPower(speed);
+            robot.rightMotor.setPower(speed);
 
             robot.waitForTick(40);
         }
@@ -39,24 +44,49 @@ public class TestDrive extends LinearOpMode {
             robot = new RamhawkHardware();
             robot.init(hardwareMap);
             driveTime = 1.0;
+            speed = 0.5;
             yBefore = false;
             aBefore = false;
+            lbBefore = false;
+            rbBefore = false;
+            index = true;
             init = true;
         }
 
-        telemetry.addData("Drive Time", driveTime);
+        telemetry.addData((index ? "--> " : "    ") + "Drive Time", driveTime);
+        telemetry.addData((index ? "    " : "--> ") + "Speed", speed);
         telemetry.update();
+
+        if (gamepad1.left_bumper) {
+            if (!lbBefore) {
+                index = !index;
+                lbBefore = true;
+            }
+        } else lbBefore = false;
+
+        if (gamepad1.right_bumper) {
+            if (!rbBefore) {
+                index = !index;
+                rbBefore = true;
+            }
+        } else rbBefore = false;
 
         if (gamepad1.y) {
             if (!yBefore) {
-                driveTime += 0.05;
+                if (index)
+                    driveTime += 0.05;
+                else
+                    speed += 0.05;
                 yBefore = true;
             }
         } else yBefore = false;
 
         if (gamepad1.a) {
             if (!aBefore) {
-                driveTime -= 0.05;
+                if (index)
+                    driveTime -= 0.05;
+                else
+                    speed -= 0.05;
                 aBefore = true;
             }
         } else aBefore = false;
