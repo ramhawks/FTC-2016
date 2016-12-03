7f1d504c268208e5fc2ea25350b47b3f0f833a1c
Winona Ramhawks <winonaramhawks@gmail.com>
2016-12-03 14:46:48 -0600

    Fixed Autonomous

---
+++ main/java/org/firstinspires/ftc/teamcode/KnockCapBall.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by Team3090 on 12/3/2016.
+ */
+@Autonomous(name = "KnockCapBall", group = "Main")
+public class KnockCapBall {
+    private RamhawkHardware robot;
+    private ElapsedTime timer;
+
+
+}
